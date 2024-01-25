ca0ea74e467870d0d3c0aaf1c969212ec7db5f5a
test-audify <157669556+test-audify@users.noreply.github.com>
2024-01-25 13:35:04 +0700

    up

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -32,9 +32,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "01dec05d10c7d25c7fff3d7cc6143dae
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "fb4a28e0169eaa80633cfb6246479715",
-"/": "fb4a28e0169eaa80633cfb6246479715",
-"main.dart.js": "34e5b9ac0cc9af1b591d23b143dec56e",
+"index.html": "d3565147a270fa05dd63f12e9980d562",
+"/": "d3565147a270fa05dd63f12e9980d562",
+"main.dart.js": "23c6ee8f07849ef0517a1421ea8a6a06",
 "manifest.json": "dfd4934753a3a03a408caa48f23e9dea",
 "version.json": "f06744fae09becc51136d51802a47d53"};
 // The application shell files that are downloaded before a service worker can
